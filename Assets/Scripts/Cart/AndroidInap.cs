3bd9d8df491f21ada856c03508ed686d6de66741
trimf <a.b@i-free.com>
2013-06-20 12:45:33 +0400

    keyaliase password

--- Assets/Scripts/Cart/AndroidInap.cs
+++ Assets/Scripts/Cart/AndroidInap.cs
@@ -6,7 +6,7 @@ public class AndroidInap : Abstract, IInap{
 	private string key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkUZfw6OPqYywVYwazU4AYOiI7yH0UGPC9U/SpWGKrviczp66r67Lfb22J9Q9iQKzbzinEJBdnJdSZyWJYuFTF4WGUvQzqb9MdMWhGg+FaNEv4kbDlkUWL+FJi1JsM5JrPEFQOP38sBiFl8RHvBJOmNsoDo9zkUHRV49DKbSE5ycZjms6RAqcKTXYmh0EYgo5IECmoNKSi5GCRCZjhFQTWYrOFTisQtrzrrPkuB9ALjOLriSt3snh9FCQXLbH7Nnab0mRxEgog8h2v5MYefzJYdA0/hZKq7EPOPIzV8HTkWy9Rc1N+VtFwp+x40Gkul3+CNL8m/ZVG/fpk7ROwGf5wQIDAQAB";
 	
 	public static string[] inapKeys = new string[] { 
-		"android.test.purchased",
+		"coins_pack1",
 		"coins_pack2",
 		"coins_pack3",
 		"coins_pack4",
